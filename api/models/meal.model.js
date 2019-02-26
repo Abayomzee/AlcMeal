142a1017b32399e71e173be7a4d31937d0727128
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2019-02-26 15:50:53 +0100

    g
    s

--- api/models/meal.model.js
+++ api/models/meal.model.js
@@ -1,22 +1,10 @@
-"use strict";
+class Meal {
+    constructor() {
+        this.id = null;
+        this.name = null;
+        this.size = null;
+        this.price = null;
+    }
+}
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
-function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return right[Symbol.hasInstance](left); } else { return left instanceof right; } }
-
-function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Meal = function Meal() {
-  _classCallCheck(this, Meal);
-
-  this.id = null;
-  this.name = null;
-  this.size = null;
-  this.price = null;
-};
-
-var _default = Meal;
-exports.default = _default;
+export default Meal;
