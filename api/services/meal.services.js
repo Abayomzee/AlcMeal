9cfdbeb49916bed3961e259e71f09fd6053dbc16
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2019-02-26 15:55:49 +0100

    b
    a

--- api/services/meal.services.js
+++ api/services/meal.services.js
@@ -1,91 +1,76 @@
-"use strict";
+import dummy from '../utils/dummyData';
+import Meal from '../models/meal.model';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
+const MealServices = {
+    fetchAllMeals(){
+      const allMeals = dummy.meals.map( (meals) => {
+            const newMeal = new Meal();
+            newMeal.id = meals.id;
+            newMeal.name = meals.name;
+            newMeal.price = meals.price;
+            newMeal.size = meals.size;
 
-var _dummyData = _interopRequireDefault(require("../utils/dummyData"));
+            return newMeal;
+        });
 
-var _meal = _interopRequireDefault(require("../models/meal.model"));
+        return allMeals;
+    },
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+    addMeal(meal){
+        const mealLength = dummy.meals.length;
+        const lastId = dummy.meals[mealLength - 1].id;
+        const newId = lastId + 1;
 
-var MealServices = {
-  fetchAllMeals: function fetchAllMeals() {
-    var allMeals = _dummyData.default.meals.map(function (meals) {
-      var newMeal = new _meal.default();
-      newMeal.id = meals.id;
-      newMeal.name = meals.name;
-      newMeal.price = meals.price;
-      newMeal.size = meals.size;
-      return newMeal;
-    });
+        meal.id = newId;
+        dummy.meals.push(meal);
+        return meal;
+    },
 
-    return allMeals;
-  },
-  addMeal: function addMeal(meal) {
-    var mealLength = _dummyData.default.meals.length;
-    var lastId = _dummyData.default.meals[mealLength - 1].id;
-    var newId = lastId + 1;
-    meal.id = newId;
+    getMeal(id){
+        const meal = dummy.meals.find((meal) => meal.id == id);
+        return meal || {};
+    },
 
-    _dummyData.default.meals.push(meal);
+    deleteMeal(id){
+        const meal = dummy.meals.find((meal) => meal.id == id);
+        const mealIndex = dummy.meals.indexOf(meal);
+        const removeMeal = dummy.meals.splice(mealIndex, 1);
+        // console.log(removeMeal);
+        // console.log(mealIndex);
 
-    return meal;
-  },
-  getMeal: function getMeal(id) {
-    var meal = _dummyData.default.meals.find(function (meal) {
-      return meal.id == id;
-    });
+        return removeMeal || {};
+    },
 
-    return meal || {};
-  },
-  deleteMeal: function deleteMeal(id) {
-    var meal = _dummyData.default.meals.find(function (meal) {
-      return meal.id == id;
-    });
+    updateMeal(meal, id){
 
-    var mealIndex = _dummyData.default.meals.indexOf(meal);
+        // get the meal id and index
+        const mealId = dummy.meals.find((Ameal) => Ameal.id == id);
+        const getMealIndex = dummy.meals.indexOf(mealId);
+        
+        mealId.id = id;
 
-    var removeMeal = _dummyData.default.meals.splice(mealIndex, 1); // console.log(removeMeal);
-    // console.log(mealIndex);
+        if(meal.name == null){
+            mealId.name = mealId.name;
+        } else {
+            mealId.name = meal.name;
+        }
 
+        if(meal.size == null){
+            mealId.size = mealId.size;
+        } else {
+            mealId.size = meal.size;
+        }
 
-    return removeMeal || {};
-  },
-  updateMeal: function updateMeal(meal, id) {
-    // get the meal id and index
-    var mealId = _dummyData.default.meals.find(function (Ameal) {
-      return Ameal.id == id;
-    });
+        if(meal.price == null){
+            mealId.price = mealId.price;
+        } else {
+            mealId.price = meal.price;
+        }
 
-    var getMealIndex = _dummyData.default.meals.indexOf(mealId);
+        dummy.meals.splice(getMealIndex, 1, mealId);
 
-    mealId.id = id;
-
-    if (meal.name == null) {
-      mealId.name = mealId.name;
-    } else {
-      mealId.name = meal.name;
+        return mealId;
     }
+}
 
-    if (meal.size == null) {
-      mealId.size = mealId.size;
-    } else {
-      mealId.size = meal.size;
-    }
-
-    if (meal.price == null) {
-      mealId.price = mealId.price;
-    } else {
-      mealId.price = meal.price;
-    }
-
-    _dummyData.default.meals.splice(getMealIndex, 1, mealId);
-
-    return mealId;
-  }
-};
-var _default = MealServices;
-exports.default = _default;
+export default MealServices;
