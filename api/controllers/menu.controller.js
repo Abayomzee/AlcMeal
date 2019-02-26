56fae434d0bf823d8d3d66da0a23693f58d5a8a2
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2019-02-26 15:49:12 +0100

    d
    s

--- api/controllers/menu.controller.js
+++ api/controllers/menu.controller.js
@@ -1,33 +1,23 @@
-"use strict";
+import MenuServices from '../services/menu.services';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
+const MenuController = {
+    fetchAllMenus(req, res){
+        const allMenus = MenuServices.fetchAllMenus();
+        return res.json({
+            status: 'Success',
+            data: allMenus
+        }).status(200);
+    },
 
-var _menu = _interopRequireDefault(require("../services/menu.services"));
+    setupMenu(req, res){
+        const getMenu = req.body;
+        const createdMenu = MenuServices.setMenu(getMenu);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+        return res.json({
+            status: 'Success',
+            data: createdMenu
+        }).status(200);
+    }
+}
 
-var MenuController = {
-  fetchAllMenus: function fetchAllMenus(req, res) {
-    var allMenus = _menu.default.fetchAllMenus();
-
-    return res.json({
-      status: 'Success',
-      data: allMenus
-    }).status(200);
-  },
-  setupMenu: function setupMenu(req, res) {
-    var getMenu = req.body;
-
-    var createdMenu = _menu.default.setMenu(getMenu);
-
-    return res.json({
-      status: 'Success',
-      data: createdMenu
-    }).status(200);
-  }
-};
-var _default = MenuController;
-exports.default = _default;
+export default MenuController;
