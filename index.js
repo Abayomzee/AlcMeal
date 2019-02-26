1abd676b17ae5a71dd2d9a773029d74e8fed5424
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2019-02-26 14:50:38 +0100

    edit
    edit

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-var _express = _interopRequireDefault(require("express"));
+import express from 'express';
+import bodyParser from 'body-parser';
+import router from './api/routes/meal.routes';
+import menusRouter from './api/routes/menu.route';
+import OrderRouter from './api/routes/order.routes';
 
-var _bodyParser = _interopRequireDefault(require("body-parser"));
+const app = express();
 
-var _meal = _interopRequireDefault(require("./api/routes/meal.routes"));
+const port = 9001;
 
-var _menu = _interopRequireDefault(require("./api/routes/menu.route"));
+app.use(bodyParser.json());
 
-var _order = _interopRequireDefault(require("./api/routes/order.routes"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var app = (0, _express.default)();
-var port = 9001;
-app.use(_bodyParser.default.json());
-app.get('/', function (req, res) {
-  return res.send('App is now working fine');
+app.get('/', (req, res) => {
+    return res.send('App is now working fine');
 });
-app.use('/api/v1/meals', _meal.default);
-app.use('/api/v1/menus', _menu.default);
-app.use('/api/v1/orders', _order.default);
-app.listen(port, function () {
-  console.log("App is listening on port ".concat(port));
-});
+
+app.use('/api/v1/meals', router);
+app.use('/api/v1/menus', menusRouter)
+app.use('/api/v1/orders', OrderRouter);
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+})
+
 module.exports = app;
