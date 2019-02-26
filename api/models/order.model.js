2487df0fcdece80b9783b43b5acc6888173ab9f4
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2019-02-26 15:52:25 +0100

    b
    q

--- api/models/order.model.js
+++ api/models/order.model.js
@@ -1,22 +1,10 @@
-"use strict";
+class Orders {
+    constructor(){
+        this.id = null;
+        this.userName = null;
+        this.address = null;
+        this.items = {}
+    }
+}
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
-function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return right[Symbol.hasInstance](left); } else { return left instanceof right; } }
-
-function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Orders = function Orders() {
-  _classCallCheck(this, Orders);
-
-  this.id = null;
-  this.userName = null;
-  this.address = null;
-  this.items = {};
-};
-
-var _default = Orders;
-exports.default = _default;
+export default Orders;
