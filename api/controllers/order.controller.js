34a393ff5f4b764b8731c0ebe0276a3bb21c7913
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2019-02-26 15:49:58 +0100

    g
    b

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -1,44 +1,34 @@
-"use strict";
+import OrderService from '../services/order.service';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
+const OrderController = {
+    postOrder(req, res){
+        const orderBody = req.body;
+        const orderCreated = OrderService.postAnOrder(orderBody);
 
-var _order = _interopRequireDefault(require("../services/order.service"));
+        return res.json({
+            status: 'Success',
+            data: orderCreated
+        }).status(200);
+    },
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+    modifyOrders(req, res){
+        const orderBody = req.body;
+        const orderId = req.params.id;
+        const editOrder = OrderService.modifyOrder(orderBody, orderId);
 
-var OrderController = {
-  postOrder: function postOrder(req, res) {
-    var orderBody = req.body;
+        return res.json({
+            status: 'Success',
+            data: editOrder
+        }).status(200);
+    },
 
-    var orderCreated = _order.default.postAnOrder(orderBody);
+    fetchAllOrders(req, res){
+        const allOrders = OrderService.getAllOrders();
+        return res.json({
+            status: 'Success',
+            data: allOrders
+        }).status(200);
+    }
+}
 
-    return res.json({
-      status: 'Success',
-      data: orderCreated
-    }).status(200);
-  },
-  modifyOrders: function modifyOrders(req, res) {
-    var orderBody = req.body;
-    var orderId = req.params.id;
-
-    var editOrder = _order.default.modifyOrder(orderBody, orderId);
-
-    return res.json({
-      status: 'Success',
-      data: editOrder
-    }).status(200);
-  },
-  fetchAllOrders: function fetchAllOrders(req, res) {
-    var allOrders = _order.default.getAllOrders();
-
-    return res.json({
-      status: 'Success',
-      data: allOrders
-    }).status(200);
-  }
-};
-var _default = OrderController;
-exports.default = _default;
+export default OrderController;
